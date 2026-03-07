873efb0dcab306ac230ccb60ebadf21dfc8b9cfa
hbk <1093993119@qq.com>
2026-03-07 21:34:09 +0800

    refactor(zmodel): 移除 zmodel 模块，拆分至 ziface 与 znet - 删除 zmodel: 接口定义迁移至 ziface，消息类型迁移至 znet/netmsg.go - 删除 proto 相关文件（msg.proto/msg.pb.go/msg_vtproto.pb.go）

--- zmodel/gen_proto.bat
+++
@@ -1,4 +0,0 @@
-
-..\tools\proto\win\protoc.exe -I=. --plugin=protoc-gen-go=..\tools\proto\win\protoc-gen-go.exe --go_out=../../ --go-vtproto_out=../../ --go-vtproto_opt=features=all ./msg.proto
-
-gofmt -w .
